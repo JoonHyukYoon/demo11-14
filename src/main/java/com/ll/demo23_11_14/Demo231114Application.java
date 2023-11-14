0a12b74419d78fc91b3cd136f4fd22205a110fbb
JoonHyukYoon <crayngnuk1@naver.com>
2023-11-14 11:47:43 +0900

    setting

---
+++ src/main/java/com/ll/demo23_11_14/Demo231114Application.java
@@ -0,0 +1,13 @@
+package com.ll.demo23_11_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo231114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo231114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/demo23_11_14/Demo231114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.demo23_11_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo231114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
